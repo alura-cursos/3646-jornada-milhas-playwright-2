2326a7454ed8a28f6e4f5857b29a22c8077f2f43
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-15 14:47:48 -0300

    Vídeo 3.1 - Abordagem de otimização

---
+++ e2e/specs/edicaoDePerfil.spec.ts
@@ -0,0 +1,21 @@
+import { test } from "e2e/setup/fixtures";
+
+test.describe("Página de perfil", () => {
+  test("Editar perfil 1", () => {
+    // fazer cadastro de um novo perfil
+
+    // fazer login desse perfil
+
+    // editar esse perfil
+  });
+
+  test("Editar perfil 2", () => {
+    // fazer cadastro de um novo perfil
+
+    // fazer login desse perfil
+
+    // editar esse perfil
+  });
+
+  // 50 testes de edição de perfil -> 150 testes
+});
