4fadd0203fa09651b0a400059a6af73a4de5e186
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-15 15:15:00 -0300

    Vídeo 3.3 - Criando fixture de escopo de worker

--- e2e/specs/edicaoDePerfil.spec.ts
+++ e2e/specs/edicaoDePerfil.spec.ts
@@ -1,21 +1,9 @@
-import { test } from "e2e/setup/fixtures";
+import { testeLogado } from "e2e/setup/testeLogado";
 
-test.describe("Página de perfil", () => {
-  test("Editar perfil 1", () => {
-    // fazer cadastro de um novo perfil
-
-    // fazer login desse perfil
-
-    // editar esse perfil
+testeLogado.describe("Página de perfil", () => {
+  testeLogado("Editar perfil 1", () => {
   });
 
-  test("Editar perfil 2", () => {
-    // fazer cadastro de um novo perfil
-
-    // fazer login desse perfil
-
-    // editar esse perfil
+  testeLogado("Editar perfil 2", () => {
   });
-
-  // 50 testes de edição de perfil -> 150 testes
 });
