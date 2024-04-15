df37025a4b4e7edfda50b62463b3c9c2da6f595a
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-15 12:31:10 -0300

    Mão na massa: interagindo com página de cadastro

--- e2e/specs/cadastro.spec.ts
+++ e2e/specs/cadastro.spec.ts
@@ -7,7 +7,18 @@ test.describe('Página de cadastro', () => {
 
     const novoUsuario = gerarPerfil();
 
-    console.log(novoUsuario);
-    
+    await paginaCadastro.visitar();
+    await paginaCadastro.definirNome(novoUsuario.nome);
+    await paginaCadastro.definirDataNascimento(novoUsuario.dataNascimento);
+    await paginaCadastro.definirCPF(novoUsuario.cpf);
+    await paginaCadastro.definirTelefone(novoUsuario.telefone);
+    await paginaCadastro.definirCidade(novoUsuario.cidade);
+    await paginaCadastro.definirEstado(novoUsuario.estado);
+
+    await paginaCadastro.definirEmail(novoUsuario.email);
+    await paginaCadastro.confirmarEmail(novoUsuario.email);
+    await paginaCadastro.definirSenha(novoUsuario.senha);
+    await paginaCadastro.confirmarSenha(novoUsuario.senha);
+    await paginaCadastro.confirmarTermos();
   });
 });
