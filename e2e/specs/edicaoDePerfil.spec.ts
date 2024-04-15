28ac38ade6318830e971ade3a867db6f1d5406f6
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-15 15:32:32 -0300

    Vídeo 3.4 - Usando arquivos de login

--- e2e/specs/edicaoDePerfil.spec.ts
+++ e2e/specs/edicaoDePerfil.spec.ts
@@ -1,7 +1,8 @@
 import { testeLogado } from "e2e/setup/testeLogado";
 
 testeLogado.describe("Página de perfil", () => {
-  testeLogado("Editar perfil 1", () => {
+  testeLogado("Editar perfil 1", async ({ paginaPrincipal }) => {
+    await paginaPrincipal.visitar();
   });
 
   testeLogado("Editar perfil 2", () => {
