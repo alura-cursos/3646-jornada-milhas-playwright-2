cbc53694459e587dd0863bd1afd6e2c200ce9338
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-16 16:12:18 -0300

    fix: Adicionando paginaLogin.visitar()

--- e2e/specs/fazerLogin.spec.ts
+++ e2e/specs/fazerLogin.spec.ts
@@ -15,6 +15,7 @@ test.describe("Página de Login", () => {
   });
 
   test("Não deve conseguir fazer login com email inválido", async ({ paginaLogin }) => {
+    await paginaLogin.visitar();
     await paginaLogin.fazerLogin('antonio.errado@alura.com', '123456');
     await paginaLogin.estaMostrandoMensagemDeErro('Você não está autorizado a acessar este recurso');
   });
