cde8882771e6a5d0637f8aafa5653c54a80b6ae4
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-16 10:50:49 -0300

    Vídeo 4.3 - Page Object da página de perfil

--- e2e/setup/fixtures.ts
+++ e2e/setup/fixtures.ts
@@ -2,11 +2,13 @@ import { test as base } from "@playwright/test";
 import PaginaPrincipal from "e2e/page-objects/PaginaPrincipal";
 import PaginaLogin from "e2e/page-objects/PaginaLogin";
 import PaginaCadastro from "e2e/page-objects/PaginaCadastro";
+import PaginaPerfil from "e2e/page-objects/PaginaPerfil";
 
 export const test = base.extend<{
   paginaPrincipal: PaginaPrincipal,
   paginaLogin: PaginaLogin,
-  paginaCadastro: PaginaCadastro
+  paginaCadastro: PaginaCadastro,
+  paginaPerfil: PaginaPerfil
 }>({
   paginaPrincipal: async ({ page }, use) => {
     const paginaPrincipal = new PaginaPrincipal(page);
@@ -19,5 +21,9 @@ export const test = base.extend<{
   paginaCadastro: async ({ page }, use) => {
     const paginaCadastro = new PaginaCadastro(page);
     await use(paginaCadastro);
+  },
+  paginaPerfil: async ({ page }, use) => {
+    const paginaPerfil = new PaginaPerfil(page);
+    await use(paginaPerfil);
   },
 });
